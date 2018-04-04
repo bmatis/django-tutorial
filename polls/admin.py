8849ed8f47f9817756602326da0bede06c69ff9a
bmatis <bmatis@mac.com>
2018-04-04 11:27:30 -0700

    Question Admin: Added date filtering, date hierarchy, search, and default sort.

--- polls/admin.py
+++ polls/admin.py
@@ -16,6 +16,10 @@ class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInLine]
 
     list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    date_hierarchy = 'pub_date'
+    search_fields = ['question_text']
+    ordering = ['-pub_date']
 
 admin.site.register(Question, QuestionAdmin)
 # admin.site.register(Choice)
