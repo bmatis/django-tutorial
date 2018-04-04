521d1950cfd882b73ad4349868ec5d0655406563
bmatis <bmatis@mac.com>
2018-04-04 11:01:11 -0700

    Updated Question display in admin tool to show choices inline.

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,                  {'fields': ['question_text']}),
+        ('Date Information',    {'fields': ['pub_date'],
+                                'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInLine]
+
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
