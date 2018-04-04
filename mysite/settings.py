5f67623b4b3a5c5b7abba37cbedb18d8635720fb
bmatis <bmatis@mac.com>
2018-04-03 21:03:31 -0700

    terminology update in settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '---store_in_custom_settings---'
+SECRET_KEY = '---store_in_secret_settings---'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
